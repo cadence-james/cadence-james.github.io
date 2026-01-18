61ced5cce149b49a941418a4d43fc19ea566a6a9
Cadence <142916993+cadence-james@users.noreply.github.com>
2026-01-18 02:16:50 +0000

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -21,7 +21,6 @@ export const defaultContentPageLayout: PageLayout = {
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
