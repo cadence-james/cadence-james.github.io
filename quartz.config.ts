72686f4edbbee3d63952ffe6d0e6013c36af8a73
Cadence <142916993+cadence-james@users.noreply.github.com>
2026-01-18 02:16:32 +0000

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,32 +23,32 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Spectral SC",
+        body: "Spectral",
         code: "IBM Plex Mono",
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#fff23688",
+          light: "#15063c", //Background
+          lightgray: "#000000", //Code Background
+          gray: "#b8b8b8", //"Search"
+          darkgray: "#f6d182", //Text, Mag. Glass
+          dark: "#be78ff", //Code, link icon
+          secondary: "#3c9fe1", //Links (and headers)
+          tertiary: "#12d8a7", //Hovered link
+          highlight: "rgba(255, 255, 255, 0.15)", //?
+          textHighlight: "#ffffff88", //?
         },
         darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
-          gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#b3aa0288",
+          light: "#15063c", //Background
+          lightgray: "#000000", //Code Background
+          gray: "#b8b8b8", //"Search"
+          darkgray: "#f6d182", //Text, Mag. Glass
+          dark: "#be78ff", //Code, link icon
+          secondary: "#3c9fe1", //Links (and headers)
+          tertiary: "#12d8a7", //Hovered link
+          highlight: "rgba(255, 255, 255, 0.15)", //?
+          textHighlight: "#ffffff88", //?
         },
       },
     },
